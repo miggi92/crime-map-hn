ca02ad3e9d1ac76090b795e789d3d2c35b536f88
Miguel <miggi92@users.noreply.github.com>
2025-04-16 16:18:40 +0000

    feat: :sparkles: added header content

--- app/components/app/header.vue
+++ app/components/app/header.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-    
-  </div>
+  <UHeader>
+    <template #title>
+      <Logo class="h-6 w-auto" />
+    </template>
+
+    <UNavigationMenu :items="items" />
+
+    <template #right>
+      <UColorModeButton />
+
+      <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
+        <UButton
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/miggi92/crime-map-hn"
+          target="_blank"
+          icon="i-simple-icons-github"
+          aria-label="GitHub"
+        />
+      </UTooltip>
+    </template>
+  </UHeader>
 </template>
 
 <script lang="ts" setup>
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Map',
+    to: '/map',
+    icon: 'i-lucide:map',
+  }])
 
 </script>
 
-<style>
-
-</style>
+<style></style>
