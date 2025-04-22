8990e72404518c0b2d3682e1083b11a9958bbec6
Miguel <miggi92@users.noreply.github.com>
2025-04-22 14:05:13 +0000

    feat: :sparkles: return feed as json

---
+++ server/api/prp/news.ts
@@ -0,0 +1,29 @@
+import { parseString } from 'xml2js';
+
+export default defineEventHandler(async (event) => {
+  const rssUrl = 'https://www.presseportal.de/rss/dienststelle_110971.rss2'
+
+  try {
+    // RSS-Feed abrufen
+    const responseXML = await $fetch(rssUrl)
+
+    let jsonData: string = '';
+    parseString(responseXML as string, (err, result) => {
+      if (err) {
+        throw createError({
+          statusCode: 500,
+          data: err,
+          statusMessage: 'Error parsing RSS feed',
+        });
+      }
+      jsonData = result;
+    });
+    return jsonData.rss.channel;
+  } catch (error) {
+    throw createError({
+      statusCode: 500,
+      data: error,
+      statusMessage: 'Error fetching RSS feed',
+    })
+  }
+});
